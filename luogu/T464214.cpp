f3ed327ddf946f33197ce80e3883d555b489899f
big6s <2054293697@qq.com>
2024-08-18 19:22:00 +0800

    update

--- luogu/T464214.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-int main() {
-	long long x,y;
-	cin>>x>>y;
-	cout<<(y-x+1)/2;
-	return 0;
-}
--- luogu/T485506.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <cstring>
-
-using namespace std;
-
-int main() {
-	cout << "CDDBABABCDTFFTCB" << endl;
-	return 0;
-}
